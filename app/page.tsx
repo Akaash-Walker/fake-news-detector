67585b5ab4d45c98bdb5b4e2c4b4caa8e137e3d6
Akaash-Walker <awalker7179@gmail.com>
2026-05-13 19:31:06 -0400

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,11 +39,6 @@ export default function Home() {
 
     return (
         <div className="relative flex h-screen w-screen items-center justify-center overflow-hidden">
-            <div
-                className="absolute inset-0 z-0 flex items-center justify-center opacity-25 scale-75 pointer-events-none">
-                <img src="/wpi_roundel.png" alt="WPI roundel"/>
-            </div>
-
             <div className="relative flex flex-col items-center gap-6 md:gap-24">
                 <p
                     className="w-full text-center text-6xl font-bold leading-tight">
